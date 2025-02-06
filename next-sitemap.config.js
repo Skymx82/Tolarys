b40c21688fca0a20a825f04f61bfbf03b85f215a
Mattias Mathevon <mattiasmathevon@MacBook-Air-de-Mattias.local>
2025-02-06 14:06:12 +0100

    Correction header et sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://tolarys.fr',
+  siteUrl: 'https://www.tolarys-toulouse.fr',
   generateRobotsTxt: true,
   sitemapSize: 7000,
   exclude: ['/404'],
   robotsTxtOptions: {
     additionalSitemaps: [
-      'https://tolarys.fr/sitemap.xml',
+      'https://www.tolarys-toulouse.fr/sitemap.xml',
     ],
   },
 }

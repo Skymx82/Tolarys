f17374d363649aba466ed3ad45061dcfb24de7af
Mattias Mathevon <mattiasmathevon@MacBook-Air-de-Mattias.local>
2025-02-07 15:58:41 +0100

    CHangement vitesse des Techno

--- src/components/TechScroll.tsx
+++ src/components/TechScroll.tsx
@@ -53,7 +53,7 @@ const technologies: Technology[] = [
 const TechScroll: React.FC = () => {
   return (
     <div className="w-full overflow-hidden py-12">
-      <div className="flex space-x-8 animate-scroll">
+      <div className="flex space-x-8 animate-scroll sm:animate-faster">
         {[...technologies, ...technologies].map((tech, index) => (
           <div
             key={`${tech.name}-${index}`}
